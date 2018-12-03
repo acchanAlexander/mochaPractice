771aa3286b067fcef1dd405c2c2ea537b555ffed
acchanAlexander <xxxg_01_w_endless_waltz@yahoo.co.jp>
2018-12-03 21:45:58 +0900

    Create product code

---
+++ fizzbuzz.js
@@ -0,0 +1,27 @@
+'use strict'
+
+const max = 30;
+
+function fizzbuzz(args) {
+  try {
+    if (typeof args !== 'number') {
+      throw new Error("type is not number")
+    }
+    
+    for(let i = 1; i < args + 1; i++) {
+      if (i % 3 === 0 && i % 5 === 0) {
+        console.log('fizzbuzz');
+      } else if (i % 3 === 0) {
+        console.log('fizz');
+      } else if (i % 5 === 0) {
+        console.log('buzz');
+      } else {
+        console.log(i);
+      }
+    }
+  } catch(err) {
+    console.log(err);
+  }
+}
+
+fizzbuzz(max);
