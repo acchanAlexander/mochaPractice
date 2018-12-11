a9280c63c780b4774dbfd3aadeb157b717fe6e34
acchanAlexander <xxxg_01_w_endless_waltz@yahoo.co.jp>
2018-12-11 21:16:19 +0900

    edit product code

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,25 +1,29 @@
 'use strict'
 
 function fizzbuzz(args) {
-  try {
-    if (typeof args !== 'number') {
-      throw new Error('type is not number');
-    }
-    
-    for(let i = 1; i < args + 1; i++) {
-      if (i % 3 === 0 && i % 5 === 0) {
-        console.log('fizzbuzz');
-      } else if (i % 3 === 0) {
-        console.log('fizz');
-      } else if (i % 5 === 0) {
-        console.log('buzz');
-      } else {
-        console.log(i);
-      }
-    }
-  } catch(err) {
-    console.log(err);
+  if (!isValidArgs(args)) {
+    return;
   }
+
+  for(let i = 1; i < args + 1; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log('fizzbuzz');
+    } else if (i % 3 === 0) {
+      console.log('fizz');
+    } else if (i % 5 === 0) {
+      console.log('buzz');
+    } else {
+      console.log(i);
+    }
+  }
+}
+
+function isValidArgs(args) {
+  if (typeof args !== 'number') {
+    console.log('args is not number')
+    return false;
+  }
+  return true;
 }
 
 module.exports = fizzbuzz;
