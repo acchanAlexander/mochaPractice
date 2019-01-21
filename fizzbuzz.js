61015da5cedf05ee91372458610a7f4da16e9256
acchanAlexander <xxxg_01_w_endless_waltz@yahoo.co.jp>
2019-01-21 23:20:18 +0900

    fine modification and creating test code

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,29 +1,31 @@
 'use strict'
 
-function fizzbuzz(args) {
-  if (!isValidArgs(args)) {
-    return;
-  }
-
-  for(let i = 1; i < args + 1; i++) {
-    if (i % 3 === 0 && i % 5 === 0) {
-      console.log('fizzbuzz');
-    } else if (i % 3 === 0) {
-      console.log('fizz');
-    } else if (i % 5 === 0) {
-      console.log('buzz');
-    } else {
-      console.log(i);
+const foo = {
+  fizzbuzz: (args) => {
+    if (!foo.isValidArgs(args)) {
+      return null;
     }
+
+    for(let i = 1; i < args + 1; i++) {
+      if (i % 3 === 0 && i % 5 === 0) {
+        //console.log('fizzbuzz');
+      } else if (i % 3 === 0) {
+        //console.log('fizz');
+      } else if (i % 5 === 0) {
+        //console.log('buzz');
+      } else {
+        //console.log(i);
+      }
+    }
+  },
+  isValidArgs: (args) => {
+    if (typeof args !== 'number') {
+      console.log('args is not number')
+      return false;
+    }
+    return true;
   }
+
 }
 
-function isValidArgs(args) {
-  if (typeof args !== 'number') {
-    console.log('args is not number')
-    return false;
-  }
-  return true;
-}
-
-module.exports = fizzbuzz;
+module.exports = foo;
--- test/fizzbuzz_test.js
+++ test/fizzbuzz_test.js
@@ -1,15 +1,33 @@
 'use strict'
 
 const chai = require('chai');
+const sinon = require('sinon');
 const expect = chai.expect;
-const fizzbuzz = require('../fizzbuzz')
+const foo = require('../fizzbuzz')
 
-describe('#fizzbuzz', () => {
+describe('foo', () => {
 
-  describe('args is not type of number', () => {
+  beforeEach(() => {
+    const fizzbuzzSpy = sinon.spy(foo, 'fizzbuzz');
+    const isValidArgsSpy = sinon.spy(foo, 'isValidArgs');
+  });
 
-    it('throw Error', () => {
-      expect(fizzbuzz('foo')).to.throw('args is not number type');
+  afterEach(() => {
+    this.fizzbuzzSpy.restore();
+    this.isValidArgsSpy.restore();
+  });
+
+  describe('#fizzbuzz', () => {
+
+    it('call count', () => {
+      foo.fizzbuzz(10);
+      expect(fizzbuzzSpy.calledOnce).to.be.true;
+    });
+
+    it('isValidArgs call count', () => {
+      foo.fizzbuzz(10);
+      //console.log(isValidArgsSpy);
+      //expect(isValidArgsSpy.calledOnce).to.be.true;
     })
 
   });
